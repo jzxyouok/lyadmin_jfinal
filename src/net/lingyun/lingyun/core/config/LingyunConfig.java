f0e7d371e75c991a0dc45e7a31b4ce64ec78c96b
jry <598821125@qq.com>
2017-08-24 10:26:34 +0800

    Hello World !

--- src/net/lingyun/lingyun/core/config/LingyunConfig.java
+++ src/net/lingyun/lingyun/core/config/LingyunConfig.java
@@ -8,7 +8,7 @@ public class LingyunConfig extends JFinalConfig {
 		me.setDevMode(true);
 	}
 	public void configRoute(Routes me) {
-		me.add("/index", net.lingyun.lingyun.core.home.IndexController.class);
+		me.add("/", net.lingyun.lingyun.core.home.IndexController.class);
 		me.add("/admin/index", net.lingyun.lingyun.core.admin.IndexController.class);
 		me.add("/admin/login", net.lingyun.lingyun.core.admin.LoginController.class);
 	}
